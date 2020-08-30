ac54689e28fd7d24bd09dea43ec4082bfd611697
donaldking <donaldk@objecttechgroup.com>
2020-08-30 03:23:24 +0100

    Added JSON Data. Added Group struct

---
+++ CampRoute/Models/Group.swift
@@ -0,0 +1,18 @@
+//
+//  Group.swift
+//  CampRoute
+//
+//  Created by Donald King on 30/08/2020.
+//  Copyright © 2020 Donald King. All rights reserved.
+//
+
+import Foundation
+
+struct Group {
+    var familyid = Int()
+    var nPeople = Int()
+    var destinationCaravanNumber = Int()
+    var travelTime = Int()
+    var waitingTime = Int()
+    var route = String()
+}
